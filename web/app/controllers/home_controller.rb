b49170d218e8c86a07bdcb364e34efb23676c0d7
Tygh Walters <tyghwalters@gmail.com>
2024-10-11 14:33:04 -0700

    Replace React Frontend with Stimulus
    Included removing react files, modifying home controller, and adding an index view to home controller. Followed the official guide at https://github.com/Shopify/shopify-app-template-ruby/wiki/Replacing-React-frontend-with-Stimulus#2-load-stimulus-javascript - very easy

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -5,17 +5,20 @@ class HomeController < ApplicationController
   include ShopifyApp::EnsureInstalled
   include ShopifyApp::ShopAccessScopesVerification
 
-  DEV_INDEX_PATH = Rails.root.join("frontend")
-  PROD_INDEX_PATH = Rails.public_path.join("dist")
+  # From React template: 
+    # DEV_INDEX_PATH = Rails.root.join("frontend")
+    # PROD_INDEX_PATH = Rails.public_path.join("dist")
+  # Added from: https://github.com/Shopify/shopify-app-template-ruby/wiki/Replacing-React-frontend-with-Stimulus#1-detach-react-frontend
+  layout 'embedded_app'
 
   def index
     if ShopifyAPI::Context.embedded? && (!params[:embedded].present? || params[:embedded] != "1")
       redirect_to(ShopifyAPI::Auth.embedded_app_url(params[:host]), allow_other_host: true)
-    else
-      contents = File.read(File.join(Rails.env.production? ? PROD_INDEX_PATH : DEV_INDEX_PATH, "index.html"))
-      contents.sub!("%VITE_SHOPIFY_API_KEY%", ShopifyApp.configuration.api_key)
+    # else
+    #   contents = File.read(File.join(Rails.env.production? ? PROD_INDEX_PATH : DEV_INDEX_PATH, "index.html"))
+    #   contents.sub!("%VITE_SHOPIFY_API_KEY%", ShopifyApp.configuration.api_key)
 
-      render(plain: contents, content_type: "text/html", layout: false)
-    end
+    #   render(plain: contents, content_type: "text/html", layout: false)
+    # end
   end
 end
